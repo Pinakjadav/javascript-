ae9095ba8b1a1c0a37339edfc259d7a373ef72fe
Pinak Jadav <jadavpinaka@gmail.com>
2026-06-07 16:05:17 +0000

    array methods

---
+++ 02_basics/arrayss.js
@@ -0,0 +1,42 @@
+// ARRAYSS
+
+const myArr = [0,1,2,3,4,5]
+const myHeros = ["shaktiman","bheem","chutki"]
+
+// const myArr2 = new Array (1, 2, 3, 4)
+// console.log(myArr[1]);
+
+
+// MY ARRAYYYYYYYY
+
+// myArr.push (11)
+// myArr.push (12)
+// myArr.pop (12)
+
+// myArr.unshift(-1)
+// console.log(myArr);
+
+// myArr.shift()
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(19));
+// console.log(myArr.includes(3));
+
+// const newArr = myArr.join()
+
+// console.log(myArr)
+// console.log(typeof newArr)
+
+console.log("a",myArr);
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("b",myArr);
+
+
+const myn2 = myArr.splice(1,3)
+console.log("c",myArr);
+console.log(myn2);
+
+
+//      node 02_basics/arrayss.js
