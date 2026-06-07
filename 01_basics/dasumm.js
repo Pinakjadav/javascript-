422dd117acdb7ad2f84cf985a9bb2345b9ff8eaf
Pinak Jadav <jadavpinaka@gmail.com>
2026-06-07 11:55:44 +0000

    summary important

---
+++ 01_basics/dasumm.js
@@ -0,0 +1,28 @@
+// PRIMITIVE DATATYPES
+
+    // 7 TYPES
+        /* STRING, NUMBER, BOOLEAN, NULL, UNDEFINED, SYMBOL,BIGINT  */
+
+
+// IF LANGUAGE NO NEED TO DEFINE THE DATATYPE THAT MEAN DYNAMITICALLY LANGUAGE AND>>>>> JS IS THIS
+
+// IF ANY LANGUAGE NEED TO DEFINE THE DATATYPE IT IS KNOWN AS THE STATICALLY DEFINED LANGUAGE
+
+
+// REFRENCE TYPE (NON-PRIMITIVE DATATYPES)
+    /*      ARRAY, OBJECTS, FUNCTIONS */
+
+
+    const heroes = ["shaktiman","nagrakj", "doga"];
+    let myObj = {
+        name = "PINAK",
+        age : 22,
+
+    }
+
+
+const myFunction = function(){
+console.log("HELLOOO WORLDDD");
+}
+
+consol.log(typeof myFunction);
