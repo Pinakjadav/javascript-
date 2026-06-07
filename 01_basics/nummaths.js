6ebd038bf95ff6b1559261ab5c0a8ed54e3bccdd
Pinak Jadav <jadavpinaka@gmail.com>
2026-06-07 14:21:02 +0000

    num and math

---
+++ 01_basics/nummaths.js
@@ -0,0 +1,48 @@
+// const score = 5
+// console.log(score);
+
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// for cut the precision value it put only this kind value that after any number it puts only two digits after point...
+
+
+
+// const anotherNumber = 23.5588
+// // mean this is made the value round off and puts the data accordingly mean it counts the digits and focus on it...
+// console.log(anotherNumber.toPrecision(3));
+
+
+// const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'))
+// // en-IN mean this is setted to the indian standards and otherwise it is setted to the us standards and this is the thing which can we set by ourseld and as per our preference...
+
+///////////////////////////////++++++++++++++++++++++++++++++++++       MATHHHHH HH      +++++++++???????????????????????????////???////????/?/////////??///
+
+console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+// console.log(Math.min (4,2,3,5,8,6,9));
+// console.log(Math.max (4,2,3,5,8,6,9));
+
+
+// console.log(Math.random());
+// console.log((Math.random()*10))+1;
+// console.log(Math.floor(Math.random()*10))+1;
+
+
+
+// const min = 10 
+// const max = 20
+// console.log(Math.floor(Math.random()* (max - min + 1 ))+ min )
+
+
+
+
+// node 01_basics/nummaths.js
