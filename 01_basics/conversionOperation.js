5eaaa205476438e27d81e03305b1adc1c1c99897
Pinak Jadav <jadavpinaka@gmail.com>
2026-06-07 11:27:32 +0000

    operation

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -21,7 +21,39 @@ true >> 1, false >> 0
 
 //  1 >> TRUE ; 0 >> FALSE; "" >> FALSE; "VSD" >> TRUE
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log (stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log (stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ******************OPERATIONSSSS*********************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+
+// let str1 = "hello"
+// let str2 = "  PINAKBAPU"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log(1 + 2);
+// console.log ( "1" + 2);
+// console.log ( 1 + "2");
+// console.log (" 1"+ 2+ 2);
+// console.log ( 1 + 2 + "2");
+
+// console.log(true);
+// console.log(+true);
+// console.log(true+);
+
+let gameCounter = 100
+// ++ gameCounter;
+gameCounter++;
+console.log(gameCounter);
