5ca67620aa2f603136c75d1aa12142a4c8a1adce
Pinak Jadav <jadavpinaka@gmail.com>
2026-06-08 09:07:53 +0000

    iter

--- 04_control_flow/iterations.js
+++ 04_control_flow/iterations.js
@@ -2,7 +2,18 @@
 
 for (let i = 0; i <= 10 ; i ++){
     const element = i;
-    console.log(element);
+    // console.log(element);
+    
+}
+
+
+for (let i = 0; i <= 10; i++) {
+        console.log(`OUTER LOOP VALUE: ${i}`);
+        
+    for (let j = 0; j < j<=10  ; j++) {
+       // console.log(`INNER LOOP VALUE: ${J} AND INNER LOOP ${i}`);
+       console.log(i + '*' + ' = ' + i*j);
+    }
     
 }
 
